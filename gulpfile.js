9349012b2182fc02a788d840a6bdf4f7d1f30b8c
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-02-24 00:39:09 +0000

    add logic for purging unneeded css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 import pkgs from "gulp";
 import gulpSass from "gulp-sass";
+import purgecss from "gulp-purgecss";
 import library from "sass";
 
 const { src, dest, watch, series } = pkgs;
 const sass = gulpSass(library);
 
 function buildStyles() {
-    return src("contra/**/*.scss").pipe(sass()).pipe(dest("css"));
+    return src("contra/**/*.scss")
+        .pipe(sass())
+        .pipe(purgecss({ content: ["*.html"] }))
+        .pipe(dest("css"));
 }
 
 function watchStyles() {
-    watch(["contra/**/*.scss"], buildStyles);
+    watch(["contra/**/*.scss", "*.html"], buildStyles);
 }
 
 export default series(buildStyles, watchStyles);
